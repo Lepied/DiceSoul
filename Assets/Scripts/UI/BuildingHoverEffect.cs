c8d6c5137c10e7ec77ee1eaa2369338df467ec4a
KangHyeon <bg3049@gmail.com>
2026-02-07 04:28:30 +0900

    feat : 메인메뉴 ui개선

---
+++ Assets/Scripts/UI/BuildingHoverEffect.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BuildingHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [Header("References")]
+    [SerializeField] private Image buildingImage;
+    [SerializeField] private Outline outline;
+    
+    [Header("Color Settings")]
+    [SerializeField] private Color normalColor = Color.white;
+    [SerializeField] private Color hoverColor = new Color(1.2f, 1.2f, 1.2f, 1f);
+    
+    [Header("Scale Settings")]
+    [SerializeField] private bool useScaleEffect = false;
+    [SerializeField] private float hoverScale = 1.05f;
+    [SerializeField] private float scaleSpeed = 5f;
+    
+    private Vector3 originalScale;
+    private Vector3 targetScale;
+    private bool isHovering = false;
+    
+    void Start()
+    {
+
+        buildingImage = GetComponent<Image>();
+        outline = GetComponent<Outline>();
+        buildingImage.color = normalColor;
+        buildingImage.alphaHitTestMinimumThreshold = 0.1f;
+        outline.enabled = false;
+        
+        originalScale = transform.localScale;
+        targetScale = originalScale;
+    }
+    
+    void Update()
+    {
+        if (useScaleEffect)
+        {
+            transform.localScale = Vector3.Lerp(
+                transform.localScale, 
+                targetScale, 
+                Time.deltaTime * scaleSpeed
+            );
+        }
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isHovering = true;
+        outline.enabled = true;
+        buildingImage.color = hoverColor;
+        
+        if (useScaleEffect)
+            targetScale = originalScale * hoverScale;
+    }
+    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isHovering = false;
+        outline.enabled = false;
+        buildingImage.color = normalColor;
+        if (useScaleEffect)
+            targetScale = originalScale;
+    }
+    
+    //버튼 비활성화하거나해야할때 쓰기
+    public void ForceReset()
+    {
+        isHovering = false;
+        
+        outline.enabled = false;
+        buildingImage.color = normalColor;
+        
+        transform.localScale = originalScale;
+        targetScale = originalScale;
+    }
+}
